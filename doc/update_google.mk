738e909b27376cf8d76e102573e86e12fceeaf4d
flyingnosky <flyingnosky@163.com>
2015-10-18 10:34:10 +0800

    clear all

--- doc/update_google.mk
+++
@@ -1,9 +0,0 @@
-##How to use google in your pc
-As you know, we can't use google for some reason. But it is so important for us.
-There is a simple way that you can use it free.
-###1.copy the content from following website
-	
-	http://laod.cn/hosts/2015-google-hosts.html
-
-###2.replace file **/etc/hosts** with the content.
-Then you can use google as you can.
